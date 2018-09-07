229d780b94e5d00b95385e51942e46678527e15b
tmsah <hideki81000@gmail.com>
2018-09-07 15:43:17 -0700

    display selectedNode's ID and Name

--- src/render_root.tsx
+++ src/render_root.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 
 import "../style/index.css";
 
-import { /*JSONArray,*/ JSONObject /*, JSONValue*/ } from "@phosphor/coreutils";
+import { JSONObject } from "@phosphor/coreutils";
 
 import { ReactCytoscape } from "react-cytoscape";
 
@@ -13,9 +13,7 @@ import Button from "./Components/Buttons";
 import SimpleSelect from "./Components/layout";
 
 import Annote from "./Components/annote";
-/**
- * The properties for the JSON tree component.
- */
+
 export interface JGraph {
   //  data: string;
   elements: any;
@@ -30,13 +28,11 @@ export interface IProps {
   theme?: string;
 }
 
-/**
- * The state of the JSON tree component.
- */
 export interface IState {
   filter?: string;
   CyRef?: any;
-  selectedId?: string;
+  selectedNodeId?: string;
+  selectedNodeName?: string;
 }
 
 const cyexport = (cy:any) =>{
@@ -49,7 +45,8 @@ export class Component extends React.Component<IProps, IState> {
     super(props);
     this.state = {
       filter: "",
-      selectedId: null
+      selectedNodeId: null,
+      selectedNodeName: null
     };
   }
 
@@ -68,23 +65,17 @@ export class Component extends React.Component<IProps, IState> {
   }
 
   setEventhandlers = (cy: any) => {
-    console.log("This should be called once")
     const self = this
-    try {
       cy.on('tap', 'node', function(evt:any){
-        const selectedId: string = evt.target.id()
-        console.log( 'last tapped ', self.foo );
-        console.log( 'tapped ', selectedId, self );
-        self.foo = selectedId
-        self.setState({selectedId})
-      });} catch(e) {
-        console.log("err:" , e)
-      }
+        const selectedNodeId: string = evt.target.id()
+        const selectedNodeName: string = evt.target.data("name")
+        self.setState({selectedNodeId});
+        self.setState({selectedNodeName});
+      });
 
   }
 
   applyStyle = () =>{
-    console.log("Style invoked!!");
     this.cy.style().selector('node').style({'background-color': 'black'}).update();
   }
 
@@ -104,22 +95,23 @@ export class Component extends React.Component<IProps, IState> {
       layout={{name:'preset'}}
       style={style}
       />
-      { <div style={{ width: '200px', height: '300px', position: 'absolute', right: 0, top:0}}>
-        <div style={{ width: "100px", height: "50px",position:"fixed",bottom: "200px"}} >
-        <SimpleSelect
-        layoutHandler={this.applyLayout}
-        />
-        </div>
-        <div style={{ width: "100px", height: "50px" }}>
-        <Button styleHandler={this.applyStyle} />
-        <Annote
-        selectedNodeId={this.state.selectedId}
-        nodes={elements.nodes.length}
-        edges={elements.edges.length} 
-        />
-        </div>
-        </div> 
-      }
+      <div style={{ width: '200px', height: '300px', position: 'absolute', right: 0, top:0}}>
+      <div style={{ width: "100px", height: "50px", position:"fixed", bottom: "200px"}} >
+      <SimpleSelect
+      layoutHandler={this.applyLayout}
+      />
+      </div>
+      <div style={{ width: "100px", height: "50px" }}>
+      <Button styleHandler={this.applyStyle} />
+      <Annote
+      selectedNodeId={this.state.selectedNodeId}
+      selectedNodeName={this.state.selectedNodeName}
+      nodes={elements.nodes.length}
+      edges={elements.edges.length} 
+      />
+      </div>
+      </div> 
+
       </div>
     );
   }
