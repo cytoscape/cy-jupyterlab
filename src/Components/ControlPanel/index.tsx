061b0ab27b833091da0225bd4674a1cb3bd8ae36
tmsah <hideki81000@gmail.com>
2018-09-11 15:58:24 -0700

    display Property&Selected Panel

--- src/Components/ControlPanel/index.tsx
+++ src/Components/ControlPanel/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import TitleBar from "./TitleBar";
 import Button from "@material-ui/core/Button";
 import LayoutSelector from "./LayoutSelector";
+import SelectedPanel from "./SelectedPanel";
+import PropertyPanel from "./PropertyPanel";
 
 const ControlPanel = (props: any) => {
-  const selectedNode = props.selectedNode;
+  const selected = props.selected;
+  const elements = props.elements;
 
   let name = "Not selected";
-  if (selectedNode) {
-    name = selectedNode.name;
+  if (selected) {
+    name = selected.name;
+    console.log("seleced:",selected);
   }
 
   return (
@@ -16,7 +20,8 @@ const ControlPanel = (props: any) => {
       <TitleBar title={props.networkname} />
 
       <div>
-        <h1>SELECTION = {name}</h1>
+        <PropertyPanel elements={elements}/>
+        <SelectedPanel selected={selected}/>
       </div>
       <LayoutSelector layoutAction={props.layoutAction} />
       <div>
